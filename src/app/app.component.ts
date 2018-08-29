fd2d8b500f7c49aebf3b456b5e34f935e5887c05
Mikalai Hais <HaisM@iba>
2018-08-29 13:20:11 +0300

    OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'developer-do';
+  todoArray=[];
+  todo;
+  //todoForm: new FormGroup()
+
+   addTodo(value){
+    if(value!==""){
+     this.todoArray.push(value)
+    //console.log(this.todos) 
+  }else{
+    alert('Input your task')
+  }
+    
+  }
+
+  //delete item
+  deleteItem(todo){
+  	for(let i=0 ;i<= this.todoArray.length ;i++){
+  		if(todo == this.todoArray[i]){
+  			this.todoArray.splice(i,1)
+  		}
+  	}
+  }
+
+  // submit Form
+  todoSubmit(value:any){
+     if(value!==""){
+    this.todoArray.push(value.todo)
+     //this.todoForm.reset()
+    }else{
+      alert('Input your task')
+    }
+    
+  }
+ 
 }
